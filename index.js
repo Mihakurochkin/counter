29fe741dc8949e71153d6252ff0fa192a7247eb7
mihakurochkin <kurochkinmy@gmail.com>
2020-05-10 15:17:13 +0300

    Implement running indicator

--- index.js
+++ index.js
@@ -9,21 +9,26 @@ update();
 document.getElementById('plus').onclick = () => {
   if(isRunning) {
     counter++;
-    update();
   }
+
+  update();
 };
 
 document.getElementById('start').onclick = () => {
   isRunning = true;
-  counter = 0;
+  counter =  0;
   setTimeout(() => {
     isRunning = false;
+    update();
     alert(`Your result ${counter}`);
   },10000);
+
+  update();
 };
 
 
 // update functions
 function update(){
+  document.getElementById('page').style.backgroundColor = isRunning ? '#81ecec' : 'white';
   document.getElementById('counter').innerText = counter;
 }
